f7dd309c4385e95b448e444997600d1899ee0c09
Weston Dransfield <wdrans@gmail.com>
2022-08-25 16:27:35 -0600

    Add event model

---
+++ src/models/event.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+interface IEvent {
+  name: string;
+  type: string;
+  user_id: number;
+  date: Date;
+}
+
+const eventSchema = new Schema<IEvent>({
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  user_id: { type: Number, required: true },
+  date: { type: Date, required: true },
+});
+
+const Event = model<IEvent>("Event", eventSchema);
+
+export default Event;
