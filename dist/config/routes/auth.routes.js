d224bec0dcc1c437ad133396a52ecbddae10deb4
Weston Dransfield <wdrans@gmail.com>
2022-09-03 11:30:40 -0600

    add dist to gitignore

--- dist/config/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const auth_controller_1 = require("../../controllers/auth.controller");
-const authRoutes = (router) => {
-    router.get("/login", auth_controller_1.login);
-    return router;
-};
-exports.default = authRoutes;
