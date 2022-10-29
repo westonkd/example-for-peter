08d448ce72de05d6fa3ce7cdbd119a095111f51b
peterjwatson <peterskyguy@gmail.com>
2022-10-29 15:32:19 -0400

    auth config change

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -7,7 +7,7 @@ import { ConfigParams } from "express-openid-connect";
 const authConfig: ConfigParams = {
   authRequired: false,
   auth0Logout: true,
-  baseURL: process.env.BASE_URL || `https://cse-341-test-94u0.onrender.com:${process.env.PORT}`,
+  baseURL: process.env.BASE_URL || `https://cse-341-test-94u0.onrender.com`,
   clientID: process.env.AUTH_ZERO_CLIENT_ID || "",
   issuerBaseURL: process.env.AUTH_ZERO_URL || "",
   secret: process.env.AUTH_ZERO_CLIENT_SECRET || "",
